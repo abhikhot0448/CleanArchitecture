8e69116bdb0e318793fee59158986e746e8132a8
abhikhot1711@gmail.com <abhikhot1711@gmail.com>
2024-11-28 10:13:32 +0530

    feat: create endpoints for fetching user all and by id

---
+++ ActivityTracker.Application/Users/Queries/GetAllUserQuery.cs
@@ -0,0 +1,6 @@
+﻿using ActivityTracker.Domain.Entities;
+using MediatR;
+
+namespace ActivityTracker.Application.Users.Queries;
+
+public record GetAllUserQuery(): IRequest<IEnumerable<User>>;
---
+++ ActivityTracker.Application/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using ActivityTracker.Domain.Entities;
+using MediatR;
+
+namespace ActivityTracker.Application.Users.Queries;
+
+public record GetUserByIdQuery(Guid userId) : IRequest<User>;
+
